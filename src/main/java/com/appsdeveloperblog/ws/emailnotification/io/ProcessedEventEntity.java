29b765852b6eee80fb6878a244afc0a8bae0db3b
Newton1998 <avsimvadim@gmail.com>
2024-08-01 10:41:24 -0400

    01/08/2024

---
+++ src/main/java/com/appsdeveloperblog/ws/emailnotification/io/ProcessedEventEntity.java
@@ -0,0 +1,53 @@
+package com.appsdeveloperblog.ws.emailnotification.io;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "processed-events")
+public class ProcessedEventEntity implements Serializable {
+    private static final long serialVersionUID = 8975462317854671234L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String messageId;
+
+    @Column(nullable = false)
+    private String productId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public ProcessedEventEntity() {
+    }
+
+    public ProcessedEventEntity(String messageId, String productId) {
+        this.messageId = messageId;
+        this.productId = productId;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+}
